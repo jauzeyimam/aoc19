bd7cbfb9b55d14424d4397b84aa97e77f942c490
Jauzey Imam <jauzey@poynt.co>
2019-12-03 02:57:16 -0800

    Day 3: cleaned up

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,189 +1,87 @@
 use super::util;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Error;
+use std::iter::FromIterator;
 
 pub struct DayThree;
 
-impl super::Solution for DayThree {
-    fn solve(&self) -> Result<String, Error> {
-        let size: usize = 40000;
-        let start: usize = size / 2;
+#[derive(Hash, Debug)]
+struct Point(isize, isize);
 
-        let mut distance = std::usize::MAX;
-        let mut least_steps = std::usize::MAX;
-
-        let mut matrix = vec![vec![0; size]; size];
-
-        let input = util::read_comma_delim_str(File::open("input/3.txt")?)?;
-
-        let step_size = 1000000000;
-        let mut line = 1;
-        loop {
-            let instruction;
-            match input.get(line - 1) {
-                Some(x) => instruction = x,
-                None => break,
-            }
-            let mut steps = line * step_size;
-            let mut current_x = start;
-            let mut current_y = start;
-            for val in instruction {
-                let op = val.get(0..1).unwrap();
-                let num: usize = val.get(1..).unwrap().parse().unwrap();
-                // println!("Op: {} | Num: {}", op, num);
-                match op {
-                    "U" => {
-                        let finish = current_y + num;
-                        while current_y < finish {
-                            steps += 1;
-                            current_y += 1;
-                            let current = matrix[current_y][current_x];
-                            if current == 0 {
-                                matrix[current_y][current_x] = steps;
-                            } else if current % (line * step_size) > step_size {
-                                if current % (3 * step_size) < step_size {
-                                    // don't overwrite if it's already a sum --
-                                    // will def be less than what you're writing
-                                    break;
-                                }
-                                let curr_steps = current + steps;
-                                // println!(
-                                //     "X: {} | Y: {} | Curr: {} | CurrSteps: {}",
-                                //     current_x, current_y, current, curr_steps
-                                // );
-
-                                matrix[current_y][current_x] = curr_steps;
-                                if curr_steps < least_steps {
-                                    least_steps = curr_steps;
-                                }
-                                let x_dist: isize = current_x as isize - start as isize;
-                                let y_dist: isize = current_y as isize - start as isize;
-                                let curr_dist = x_dist.abs() as usize + y_dist.abs() as usize;
-                                if curr_dist < distance {
-                                    distance = curr_dist;
-                                    // println!("Distance changed: {}", distance);
-                                }
-                            }
-                        }
-                    }
-                    "D" => {
-                        let finish = current_y - num;
-                        while current_y > finish {
-                            steps += 1;
-                            current_y -= 1;
-                            let current = matrix[current_y][current_x];
-                            if current == 0 {
-                                matrix[current_y][current_x] = steps;
-                            } else if current % (line * step_size) > step_size {
-                                if current % (3 * step_size) < step_size {
-                                    break;
-                                }
-                                let curr_steps = current + steps;
-                                // println!(
-                                //     "X: {} | Y: {} | Curr: {} | CurrSteps: {}",
-                                //     current_x, current_y, current, curr_steps
-                                // );
-
-                                matrix[current_y][current_x] = curr_steps;
-                                if curr_steps < least_steps {
-                                    least_steps = curr_steps;
-                                }
-                                // println!("X: {} | Y: {}", current_x, current_y);
-                                let x_dist: isize = current_x as isize - start as isize;
-                                let y_dist: isize = current_y as isize - start as isize;
-                                let curr_dist = x_dist.abs() as usize + y_dist.abs() as usize;
-                                if curr_dist < distance {
-                                    distance = curr_dist;
-                                    // println!("Distance changed: {}", distance);
-                                }
-                            }
-                        }
-                    }
-                    "R" => {
-                        let finish = current_x + num;
-                        while current_x < finish {
-                            steps += 1;
-
-                            current_x += 1;
-                            let current = matrix[current_y][current_x];
-                            if current == 0 {
-                                matrix[current_y][current_x] = steps;
-                            } else if current % (line * step_size) > step_size {
-                                if current % (3 * step_size) < step_size {
-                                    break;
-                                }
-                                let curr_steps = current + steps;
-                                // println!(
-                                //     "X: {} | Y: {} | Curr: {} | CurrSteps: {}",
-                                //     current_x, current_y, current, curr_steps
-                                // );
-                                matrix[current_y][current_x] = curr_steps;
-                                if curr_steps < least_steps {
-                                    least_steps = curr_steps;
-                                }
-                                let x_dist: isize = current_x as isize - start as isize;
-                                let y_dist: isize = current_y as isize - start as isize;
-                                let curr_dist = x_dist.abs() as usize + y_dist.abs() as usize;
-                                if curr_dist < distance {
-                                    distance = curr_dist;
-                                    // println!("Distance changed: {}", distance);
-                                }
-                            }
-                        }
-                    }
-                    "L" => {
-                        let finish = current_x - num;
-                        while current_x > finish {
-                            steps += 1;
-
-                            current_x -= 1;
-                            let current = matrix[current_y][current_x];
-                            if current == 0 {
-                                matrix[current_y][current_x] = current + steps;
-                            } else if current % (line * step_size) > step_size {
-                                if current % (3 * step_size) < step_size {
-                                    break;
-                                }
-                                let curr_steps = current + steps;
-                                // println!(
-                                //     "X: {} | Y: {} | Curr: {} | CurrSteps: {}",
-                                //     current_x, current_y, current, curr_steps
-                                // );
-                                matrix[current_y][current_x] = curr_steps;
-                                if curr_steps < least_steps {
-                                    least_steps = curr_steps;
-                                }
-                                let x_dist: isize = current_x as isize - start as isize;
-                                let y_dist: isize = current_y as isize - start as isize;
-                                let curr_dist = x_dist.abs() as usize + y_dist.abs() as usize;
-                                if curr_dist < distance {
-                                    distance = curr_dist;
-                                    // println!("Distance changed: {}", distance);
-                                }
-                            }
-                        }
-                    }
-                    _ => {
-                        // println!("Something unexpected happened!");
-                    }
-                }
-            }
-            line += 1;
-        }
-
-        // print_matrix(matrix.clone());
-
-        // println!("Closest distance: {}", distance);
-        let least_steps = least_steps % (3 * step_size);
-        // println!("Least steps: {}", least_steps);
-
-        Ok(format!("Day 3a: {}\nDay 3b: {}", distance, least_steps))
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 + self.1 == other.0 + other.1
     }
 }
 
-#[allow(dead_code)]
-fn print_matrix(matrix: Vec<Vec<usize>>) {
-    for row in matrix {
-        // println!("{:?}", row);
+impl Eq for Point {}
+
+impl super::Solution for DayThree {
+    fn solve(&self) -> Result<String, Error> {
+        let input = util::read_comma_delim_str(File::open("input/3.txt")?)?;
+
+        let map_a = self.get_points(input[0].clone());
+        let map_b = self.get_points(input[1].clone());
+
+        let key_set_a: HashSet<&Point> = HashSet::from_iter(map_a.keys());
+        let key_set_b = HashSet::from_iter(map_b.keys());
+
+        let intersection = key_set_a.intersection(&key_set_b);
+
+        let p1 = intersection
+            .clone()
+            .map(|p| p.0.abs() + p.1.abs())
+            .min()
+            .unwrap();
+        let p2 = intersection
+            .clone()
+            .map(|p| map_a.get(*p).unwrap() + map_b.get(*p).unwrap())
+            .min()
+            .unwrap();
+
+        Ok(format!("Day 3a: {}\nDay 3b: {}", p1, p2))
+    }
+}
+
+impl DayThree {
+    fn get_points(&self, input: Vec<String>) -> HashMap<Point, u64> {
+        let delta_x: HashMap<&str, isize> = [("L", -1), ("R", 1), ("U", 0), ("D", 0)]
+            .iter()
+            .cloned()
+            .collect();
+        let delta_y: HashMap<&str, isize> = [("L", 0), ("R", 0), ("U", 1), ("D", -1)]
+            .iter()
+            .cloned()
+            .collect();
+
+        let mut x = 0;
+        let mut y = 0;
+        let mut length = 0;
+
+        let mut answer: HashMap<Point, u64> = HashMap::new();
+
+        for cmd in input {
+            let op = cmd.get(0..1).unwrap();
+            let num: usize = cmd.get(1..).unwrap().parse().unwrap();
+
+            assert!(delta_x.contains_key(op));
+
+            for _ in 0..num {
+                x += delta_x.get(op).unwrap();
+                y += delta_y.get(op).unwrap();
+
+                length += 1;
+
+                let point = Point(x, y);
+
+                if !answer.contains_key(&point) {
+                    answer.insert(point, length);
+                }
+            }
+        }
+
+        answer
     }
 }
